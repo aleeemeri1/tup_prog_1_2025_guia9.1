9601d40ab4f1a84a82f11f40be90388cba03b36e
aleeemeri1 <emerialejotomas@gmail.com>
2025-05-30 15:19:43 +0200

    .

--- Guia9.1/Ejercicio1.1/Program.cs
+++ Guia9.1/Ejercicio1.1/Program.cs
@@ -4,7 +4,35 @@
     {
         static void Main(string[] args)
         {
-            
+            #region declaracion del vector
+            int[] numeros;
+            int contador;
+            #endregion
+
+            #region definicion de valor
+            numeros = new int[100];
+            #endregion
+
+            #region inicializacion
+            Console.WriteLine("Ingrese el valor:");
+            int valor = Convert.ToInt32(Console.ReadLine());
+            contador = 0;
+            while (valor != -1) ;
+            {
+                numeros[contador] = valor;
+                contador++;
+
+                Console.WriteLine("Ingrese el valor:");
+                valor = Convert.ToInt32(Console.ReadLine());
+                numeros[contador] = valor;
+            }
+            #endregion
+
+            for (int n = 0; n < contador; n++)
+            {
+                Console.Write($"{numeros[n],3}");
+                Console.ReadKey();
+            }
         }
     }
 }
